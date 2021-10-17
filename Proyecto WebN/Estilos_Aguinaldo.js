bbc0d380111f7a558d5659753d652ea2738482be
Jeremias0901 <90732956+Jeremias0901@users.noreply.github.com>
2021-10-17 16:37:05 -0300

    Add files via upload

---
+++ Proyecto WebN/Estilos_Aguinaldo.js
@@ -0,0 +1,7 @@
+function Activar_Input_1er_Semestre() {
+
+    document.getElementsById("BOTON_1ER_SEM").className = "INPUT_BOTON_activado";
+    document.getElementsByTagName("body").className = "INPUT_BOTON_activado";
+    document.getElementsById("FORM").className = "FORMULARIO_activado";
+    document.getElementsById("ID_INPUT_SEMESTRE_1").className = "CL_INPUT_SEMESTRE_1_activado";
+}
