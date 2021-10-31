44c6f9c007f469767dad240f2dd451b20459af28
Jeremias0901 <90732956+Jeremias0901@users.noreply.github.com>
2021-10-31 17:18:58 -0300

    Update Calcular_Indemnizacion.js

--- Proyecto Web/Calcular_Indemnizacion.js
+++ Proyecto Web/Calcular_Indemnizacion.js
@@ -1,8 +1,7 @@
 	var DIAS_PREAVISO = 0;
 	var ANT = 0;
 	var SBRUTO = 0;
-	
-	var INDEM_ANT = 0;
+
 	var INDEM_PRE = 0;
 
 function Calcular()
@@ -11,20 +10,12 @@ function Calcular()
 	ANT = parseInt(document.getElementById('ANT').value);
 	SBRUTO = parseInt(document.getElementById('SBRUTO').value);
 
-	INDEM_ANT = SBRUTO * ANT;
+	if ((ANT < 5)&&(DIAS_PREAVISO < 30)) INDEM_PRE = SBRUTO;
 
-	if(ANT < 5)
-	{
-		if(DIAS_PREAVISO < 30) INDEM_PRE = SBRUTO;
-		else INDEM_PRE = 0;
-	}
+	else if ((ANT >= 5)&&(DIAS_PREAVISO < 60)) INDEM_PRE = 2 * SBRUTO;
+	
+	else INDEM_PRE = 0;
 
-	else if(ANT >= 5)
-	{
-		if(DIAS_PREAVISO < 60) INDEM_PRE = 2 * SBRUTO;
-		else INDEM_PRE = 0;
-	}
-
-	document.getElementById('INDEM_ANT').value = INDEM_ANT;
 	document.getElementById('INDEM_PRE').value = INDEM_PRE;
+	document.getElementById('INDEM_ANT').value = SBRUTO * ANT;
 }
